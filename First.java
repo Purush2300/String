3f49462d9f3834ccf29545342877d893c869343a
purush2300 <reddypurushothm@gmail.com>
2026-03-03 22:57:38 +0530

    Initial commit

--- First.java
+++ First.java
@@ -1,6 +1,26 @@
 public class First {
     public static void main(String[] args) {
-        String n="NAme";
-      System.out.println(n);
+        String n="JAVA";
+        String p="Python";
+        String s1=n+p;
+        String s2=n+p;
+        if(s1==s2){
+            System.out.println("reference are equal");
+        }
+        else{
+            System.out.println("reference are not equal");
+        }
+        if(s1.equals(s2)){
+            System.out.println("values are equal");
+        }
+        else{
+            System.out.println("values are not equal");
+        }
+        if(s1.equalsIgnoreCase(s2)){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
     }
 }
