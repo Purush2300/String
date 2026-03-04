7c78e35e5dead0f14a6e9f51721996976a4368c1
purush2300 <reddypurushothm@gmail.com>
2026-03-04 23:46:32 +0530

    Initial commit

--- Firts.java
+++ Firts.java
@@ -7,5 +7,8 @@ public class Firts {
         System.out.println(n.lastIndexOf("u"));
         System.out.println(n.startsWith("Purus"));
         System.out.println(n.endsWith("reddy"));
+        System.out.println(n.contains("shoth"));
+        System.out.println(n.length());
+        System.out.println(n.substring(2, 6));
     }
 }
