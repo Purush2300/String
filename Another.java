8e6717c2ab0117f102de8fd4cf1b6fbc79acf506
purush2300 <reddypurushothm@gmail.com>
2026-03-07 22:26:39 +0530

    Initial commit

---
+++ Another.java
@@ -0,0 +1,20 @@
+public class Another {
+    public static void main(String[] args) {
+        String a="aaca";
+        int n=10;
+        int count=0;
+        for (int i = 0; i <a.length(); i++) {
+            if(a.charAt(i)=='a'){
+                count++;
+            }
+        }
+        long count1=n/a.length()*count;
+        int count2=0;
+        for (int i = 0; i < n%a.length(); i++) {
+              if(a.charAt(i)=='a'){
+                count2++;
+            }
+        }
+        System.out.println(count1+count2);
+    }
+}
