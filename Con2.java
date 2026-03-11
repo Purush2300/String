7ac4c3c64701c94c0d335df6d15e7625ddd97acc
purush2300 <reddypurushothm@gmail.com>
2026-03-11 21:04:10 +0530

    Initial commit

--- Con2.java
+++ Con2.java
@@ -3,18 +3,23 @@ public class Con2 {
         int q=0;
         for (int i = 0; i <a.length(); i++) {
             char c=a.charAt(i);
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
-            c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+            if((c>='a' && c<='z') ||(c>='A' && c<='Z'))
+            {
+                if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+                c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
 
+              q++;
             }
-            else{
-                q++;
+             else{
+              
             }
+        }
+           
         }
         return q;
     }
     public static void main(String[] args) {
-        String a="theOne";
+        String a="theOne@123";
         System.out.println(Consonents(a));
     }
 }
