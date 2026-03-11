25cf294b196498cc41ae65394aefc01b4031e3e0
purush2300 <reddypurushothm@gmail.com>
2026-03-11 22:36:59 +0530

    Initial commit

---
+++ UtoL.java
@@ -0,0 +1,21 @@
+public class UtoL {
+    static void utol(String a){
+        String t="";
+        for (int i = 0; i <a.length(); i++) {
+            char c=a.charAt(i);
+            System.out.println(c+" "+i);
+            if(c>='a'&&c<='z'){
+                t=t+(char) (c-32);
+                
+            }
+            else {
+                t=t+(char)(c+32);
+            }
+        }
+        System.out.println(t);
+    }
+    public static void main(String[] args) {
+        String a="Hello";
+        utol(a);
+    }
+}
