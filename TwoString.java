378f21b96294efb8799fae3d4ab175270128d77c
purush2300 <reddypurushothm@gmail.com>
2026-03-12 21:59:19 +0530

    Initial commit

--- TwoString.java
+++ TwoString.java
@@ -2,9 +2,10 @@ public class TwoString {
     static boolean twostring(String a,String b){
       int i=0;
       int j=0;
-      while(j<b.length()){
-        if(a.charAt(i)==b.charAt(i)){
+      while(i<a.length() && j<a.length()){
+        if(b.charAt(i)==a.charAt(i)){
             j++;
+            i++;
             return true;
 
         }
