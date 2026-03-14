7c5fbadb3e091b820363a934de0799b9d681ecd2
purush2300 <reddypurushothm@gmail.com>
2026-03-14 22:48:26 +0530

    Initial commit

--- Sub2.java
+++ Sub2.java
@@ -4,11 +4,16 @@ public class Sub2 {
        int  c = b.length();
         for (int i = 0; i <=a.length()-c; i++) {
             String t="";
+      
             for (int j = i; j <c+i; j++) {
                 t=t+a.charAt(j);
             }
             if(b.equals(t)){
-                System.out.println(count++);
+                // count++;
+                // System.out.println(count);
+               
+                // return;
+                System.out.println("true");
                 return;
             }
             
@@ -21,3 +26,21 @@ public class Sub2 {
         check(a,b);
     }
 }
+
+
+// import java.util.*;
+// public class Sub2 {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int [] b=new int[a];
+      
+//         for (int i = 0; i <a; i++) {
+//            b[i]=sc.nextInt();
+//         }
+//         for (int i = 0; i < a; i++) {
+//             System.out.println(b[i]);
+//         }
+//     }
+// }
