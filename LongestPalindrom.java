9417b8fb64d62290840b916d0f80253dc8bb06a6
purush2300 <reddypurushothm@gmail.com>
2026-03-17 22:01:37 +0530

    Initial commit

---
+++ LongestPalindrom.java
@@ -0,0 +1,23 @@
+public class LongestPalindrom {
+
+    static int first(String a, String b) {
+        for (int i = 0; i <= a.length() - b.length(); i++) {
+            int j = 0;
+
+            while (j < b.length() && a.charAt(i + j) == b.charAt(j)) {
+                j++;
+            }
+
+            if (j == b.length()) {
+                return i; // match found
+            }
+        }
+        return -1; // not found
+    }
+
+    public static void main(String[] args) {
+        String a = "leetcode";
+        String b = "leet";
+        System.out.println(first(a, b));
+    }
+}
