c7d68a742ec5bde9788bad0e2aed276ef72d8735
purush2300 <reddypurushothm@gmail.com>
2026-03-27 21:17:12 +0530

    Initial commit

---
+++ Rex.java
@@ -0,0 +1,10 @@
+public class Rex {
+    public static void main(String[] args) {
+        String a="good";
+      String  t="";
+        for (int i = a.length()-1; i >=0 ; i--) {
+            t=t+a.charAt(i);
+        }
+        System.out.println(t);
+    }
+}
